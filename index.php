fa82c74ab72514ca14f43eec101cf744baf1632f
Luan Heuripedes <heuripedesmoreiradepaula@gmail.com>
2020-07-07 18:37:25 -0300

    Organizado as imagens e arquivos em suas respectivas pastas

--- index.php
+++ index.php
@@ -5,20 +5,16 @@
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <style>
-      .card-login {
-        padding: 30px 0 0 0;
-        width: 350px;
-        margin: 0 auto;
-      }
-    </style>
+    <!--Estilo customizado-->
+    <link rel="stylesheet" href="css/estilo.css">
+  
   </head>
 
   <body>
 
     <nav class="navbar navbar-dark bg-dark">
       <a class="navbar-brand" href="#">
-        <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+        <img src="img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
         App Help Desk
       </a>
     </nav>

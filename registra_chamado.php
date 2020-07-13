05d4c1cb496435229f829e373847c906b218e675
Luan Heuripedes <heuripedesmoreiradepaula@gmail.com>
2020-07-13 17:55:46 -0300

    Registrando chamados

---
+++ registra_chamado.php
@@ -0,0 +1,23 @@
+<?php   
+    //recebe os dados do formulario abrir_chamado.php
+    echo '<pre>';
+        print_r($_POST);//é um array que sao objetos manipulados pela linguagem
+    echo '</pre>';
+
+    //trabalhando na montagem do texto
+    foreach ($_POST as $indice => $valor) {
+        $_POST[$indice] = str_replace('#', '-', $valor);
+    }
+    //implode('#', $_POST)
+    $texto = implode('#', $_POST) . PHP_EOL;
+
+
+    //Persistir os dados atraves de um arquivo txt
+    $arquivo = fopen('arquivo.txt','a');  //dois parametros('nome do arquivo.txt', 'o que faremos com o arquivo ') //abrindo o arquivo
+
+    fwrite($arquivo, $texto); //dois parametros (o arquivo que abrimos, o que queremos escrever nesse arquivo); //escrevendo o texto
+
+    fclose($arquivo); //fecha o arquivo
+
+    //echo $texto;
+?>
